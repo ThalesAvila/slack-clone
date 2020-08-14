b78bc0bce5d5ccdb1560dd8a504dbc92e86a1455
Thales Avila <dev.thales.avila@gmail.com>
2020-08-13 22:46:20 -0300

    WIP: sidebar__header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
     // BEM naming convention
     <div className="app">
       <h1>Hello World {`\u{1F680}`}</h1>
-      {/* HEADER */}
       <Header />
-      {/* SIDEBAR */}
-      {/* React-Router -> Chat screen */}
+      <div className="app__body">
+        <Sidebar />
+      </div>
     </div>
   );
 }
