268500d8f28c0dfc1748f8e43b270d62804dd6f7
Thales Avila <dev.thales.avila@gmail.com>
2020-08-16 16:42:38 -0300

    Sidebar plugged to db

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -13,12 +13,21 @@ import FileCopyIcon from "@material-ui/icons/FileCopy";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
-import db from "";
+import db from "./firebase";
 
 function Sidebar() {
   const [channels, setChannels] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    db.collection("channels").onSnapshot((snapshot) => {
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      );
+    });
+  }, []);
 
   return (
     <div className="sidebar">
@@ -45,6 +54,9 @@ function Sidebar() {
       <hr />
       <SidebarOption Icon={AddIcon} title="Add Channel" />
       {/* Connect to DB and list all channels */}
+      {channels.map((channel) => (
+        <SidebarOption title={channel.name} />
+      ))}
     </div>
   );
 }
