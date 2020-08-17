93a8e693503eac62268670182bf3afc4244daf1e
Thales Avila <dev.thales.avila@gmail.com>
2020-08-16 23:30:40 -0300

    Login Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Chat from "./Chat";
+import Login from "./Login";
 
 function App() {
+  const [user, setUser] = useState();
   return (
     // BEM naming convention
     <Router>
       <div className="app">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <div className="app__body">
+              <Sidebar />
+              <Switch>
+                <Route path="/rooms/:roomId">
+                  <Chat />
+                </Route>
+                <Route path="/">
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+        )}
       </div>
     </Router>
   );
