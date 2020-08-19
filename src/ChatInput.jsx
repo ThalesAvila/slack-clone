30f57cb6d63815c40c9df811cf913cb2906022a8
Thales Avila <dev.thales.avila@gmail.com>
2020-08-18 21:33:33 -0300

    ChatInput styling

--- src/ChatInput.jsx
+++ src/ChatInput.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import db from "./firebase";
-import "./ChatInput";
+import "./ChatInput.css";
 import { useStateValue } from "./StateProvider";
 import firebase from "firebase";
 
